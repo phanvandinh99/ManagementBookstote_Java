7ab695e61d2cd399f8467667200df8f88c38c59d
phanvandinh99 <vonhanh271@gail.com>
2022-01-08 22:32:18 +0700

    Update code

---
+++ ProjectSEM2/src/Data/ConnectDB.java
@@ -0,0 +1,51 @@
+package Data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectDB {
+     String connectionString = "jdbc:sqlserver://(local):1433;databaseName=DataManagementBookStore;user=sa;password=123456;";
+    Connection conn;
+    public ConnectDB(){
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(connectionString);
+            
+            if(conn != null){
+                System.out.println("Kết nối CSDL SQL Server thành công!");
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.toString());
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+    
+   
+    public ResultSet ExcuteQueryGetTable(String cauTruyVanSQL){
+        try {
+            Statement stmt = conn.createStatement();           
+            ResultSet rs = stmt.executeQuery(cauTruyVanSQL);
+            return rs;
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+            
+        return null;
+    }
+    
+    public void ExcuteQueryUpdateDB(String cauTruyVanSQL){
+       
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(cauTruyVanSQL);
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+    
+}
--- ProjectSEM2/src/projectsem2/ProjectSEM2.java
+++ ProjectSEM2/src/projectsem2/ProjectSEM2.java
@@ -1,11 +1,14 @@
 package projectsem2;
 
 
-public class ProjectSEM2 {
 
+public class ProjectSEM2 {
+     public static  GUI.frmDangNhap frmTC=new GUI.frmDangNhap();
+     public static Data.ConnectDB connection = new  Data.ConnectDB ();
 
     public static void main(String[] args) {
-     
+       GUI.frmDangNhap frmDN= new   GUI.frmDangNhap();
+  
+       frmDN.show();
     }
-    
 }
